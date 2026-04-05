b0d1421583d201add863e0b7b9819769bc2a71c1
Po-Gr <grigorovichpk@mail.ru>
2026-04-05 17:45:34 +0300

    vkvideo POM

---
+++ src/main/java/mobile/pageObjects/VkvideoPage.java
@@ -0,0 +1,247 @@
+package mobile.pageObjects;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
+import java.util.Map;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class VkvideoPage {
+
+    private static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(20);
+    private static final Duration LOADING_GONE_TIMEOUT = Duration.ofSeconds(10);
+    private static final Duration PLAY_HIDDEN_TIMEOUT = Duration.ofSeconds(5);
+
+    private final WebDriver driver;
+
+    private final SelenideElement fastLoginView = $("#fast_login_view");
+    private final ElementsCollection fastLoginViews = $$("#fast_login_view");
+    private final SelenideElement fastLoginTertiaryBtn = $("#fast_login_tertiary_btn");
+    private final SelenideElement playerContainer = $("#playerContainer");
+    private final ElementsCollection playerContainers = $$("#playerContainer");
+    private final SelenideElement videoPlayButton = $("#video_play_button");
+    private final ElementsCollection videoPlayButtons = $$("#video_play_button");
+    private final ElementsCollection progressViews = $$("#progress_view");
+    private final SelenideElement currentProgress = $("#current_progress");
+    private final SelenideElement seekBar = $("#seek_bar");
+
+
+    public VkvideoPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public VkvideoPage openVideoByDeeplink(String url) {
+        ((JavascriptExecutor) driver).executeScript("mobile: deepLink", Map.of(
+                "url", url,
+                "package", "com.vk.vkvideo"
+        ));
+        // тут что то должно быть ожидание
+        closeFastLoginIfAppears();
+        playerContainer.shouldBe(visible, DEFAULT_TIMEOUT);
+        return this;
+    }
+
+    public VkvideoPage closeFastLoginIfAppears() {
+//        $$(fastLoginView, playerContainer)
+//                .filter(visible)
+//                .shouldHave(CollectionCondition.sizeGreaterThan(0));
+        $("#fast_login_view, #playerContainer").shouldBe(visible);
+        if (fastLoginView.isDisplayed()) {
+            fastLoginTertiaryBtn.click();
+        }
+        playerContainer.shouldBe(visible, DEFAULT_TIMEOUT);
+
+
+        if ($$("#fast_login_view").size() > 0) { // заменить
+            fastLoginTertiaryBtn.click();
+        }
+        return this;
+    }
+
+    public VkvideoPage playFirstOpenedVideo() {
+        if (videoPlayButtons.size() > 0) { // ifDisplayed??
+            videoPlayButton.click();
+            progressViews.shouldHave(size(0), LOADING_GONE_TIMEOUT);
+        }
+        return this;
+    }
+
+    public VkvideoPage pauseVideo() {
+        playerContainer.click();
+        playerContainer.click();
+        videoPlayButton.shouldBe(visible);
+        return this;
+    }
+
+    public int getCurrentProgress() {
+        return (int) Float.parseFloat(seekBar.getText());
+//        return parseSecondsFromProgressLabel(currentProgress.getText());
+    }
+
+    public VkvideoPage playVideo() {
+        videoPlayButton.click();
+        videoPlayButtons.shouldHave(size(0), PLAY_HIDDEN_TIMEOUT);
+        return this;
+    }
+
+    private static int parseSecondsFromProgressLabel(String progressText) {
+        String left = progressText.split("/")[0].trim();
+        String[] mmSs = left.split(":");
+        return Integer.parseInt(mmSs[mmSs.length - 1].trim());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package mobile.pageObjects;
+//
+//import com.codeborne.selenide.ElementsCollection;
+//import com.codeborne.selenide.SelenideElement;
+//import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.WebDriver;
+//
+//import java.time.Duration;
+//import java.util.Map;
+//
+//import static com.codeborne.selenide.CollectionCondition.size;
+//import static com.codeborne.selenide.Condition.exist;
+//import static com.codeborne.selenide.Condition.visible;
+//import static com.codeborne.selenide.Selenide.$;
+//import static com.codeborne.selenide.Selenide.$$;
+//
+///**
+// * Page Object главного сценария VK Видео.
+// * Поля {@link SelenideElement} / {@link ElementsCollection} — прокси Selenide, поиск при действии.
+// */
+//public class VkvideoMainPage {
+//
+//    private static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(20);
+//    private static final Duration LOADING_GONE_TIMEOUT = Duration.ofSeconds(10);
+//
+//    private final WebDriver driver;
+//
+//    private final SelenideElement fastLoginView = $("#fast_login_view");
+//    private final SelenideElement fastLoginTertiaryBtn = $("#fast_login_tertiary_btn");
+//    private final SelenideElement playerContainer = $("#playerContainer");
+//    private final SelenideElement videoPlayButton = $("#video_play_button");
+//    private final ElementsCollection progressViews = $$("#progress_view");
+//    private final ElementsCollection videoPlayButtons = $$("#video_play_button");
+//    private final SelenideElement currentProgress = $("#current_progress");
+//    private final SelenideElement seekBar = $("#seek_bar");
+//
+//    public VkvideoMainPage(WebDriver driver) {
+//        this.driver = driver;
+//    }
+//
+//    public VkvideoMainPage openVkVideoDeepLink(String url) {
+//        ((JavascriptExecutor) driver).executeScript("mobile: deepLink", Map.of(
+//                "url", url,
+//                "package", "com.vk.vkvideo"
+//        ));
+//        return this;
+//    }
+//
+//    public VkvideoMainPage dismissFastLoginOverlay() {
+//        fastLoginView.should(exist, DEFAULT_TIMEOUT);
+//        fastLoginTertiaryBtn.click();
+//        return this;
+//    }
+//
+//    public VkvideoMainPage waitForPlayerVisible() {
+//        playerContainer.shouldBe(visible, DEFAULT_TIMEOUT);
+//        return this;
+//    }
+//
+//    public VkvideoMainPage startPlaybackIfStopped() {
+//        if (videoPlayButtons.size() > 0) {
+//            videoPlayButton.click();
+//        }
+//        return this;
+//    }
+//
+//    public VkvideoMainPage waitUntilLoadingIndicatorGone() {
+//        progressViews.shouldHave(size(0), LOADING_GONE_TIMEOUT);
+//        return this;
+//    }
+//
+//    public VkvideoMainPage pauseByDoubleTapPlayer() {
+//        playerContainer.click();
+//        playerContainer.click();
+//        return this;
+//    }
+//
+//    public VkvideoMainPage assertPausedOverlayVisible() {
+//        videoPlayButton.shouldBe(visible);
+//        return this;
+//    }
+//
+//    public VkvideoMainPage resumePlayback() {
+//        videoPlayButton.click();
+//        return this;
+//    }
+//
+//    public VkvideoMainPage waitUntilPlayButtonHidden(Duration timeout) {
+//        videoPlayButtons.shouldHave(size(0), timeout);
+//        return this;
+//    }
+//
+//    //убрать
+//    public String getCurrentProgressText() {
+//        return currentProgress.getText();
+//    }
+//
+//    public float getSeekBarValue() {
+//        return Float.parseFloat(seekBar.getText());
+//    }
+//
+//    public int getCurrentPositionSeconds() {
+//        return parseSecondsFromProgressLabel(getCurrentProgressText());
+//    }
+//
+//    private static int parseSecondsFromProgressLabel(String progressText) {
+//        String left = progressText.split("/")[0].trim();
+//        String[] mmSs = left.split(":");
+//        return Integer.parseInt(mmSs[mmSs.length - 1].trim());
+//    }
+//}
--- src/test/java/mobile.tests/VkvideoTest.java
+++ src/test/java/mobile.tests/VkvideoTest.java
@@ -1,5 +1,7 @@
 package mobile.tests;
 
+import mobile.Utils;
+import mobile.pageObjects.VkvideoPage;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,59 +14,85 @@ import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
+import static mobile.DriverManager.getAppiumDriver;
 import static mobile.Utils.ConnectionMode.OFF;
 import static mobile.Utils.turnNetworkConnection;
 
 public class VkvideoTest extends BaseTest {
 
+//    @Test(description = "Позитивный сценарий: видео воспроизводится",
+//            groups = {"Vkvideo"})
+//    public void shouldPlaySelectedVideo() throws InterruptedException, MalformedURLException {
+//
+////        getAppiumDriver().get("https://vkvideo.ru/video-211229778_456240419");
+//
+//        ((JavascriptExecutor) driver).executeScript("mobile: deepLink", Map.of(
+//                "url", "https://vkvideo.ru/video-211229778_456240419",
+//                "package", "com.vk.vkvideo"
+//        ));
+//
+////        if ($$("#fast_login_view").size() > 0) {
+////            $("#fast_login_tertiary_btn").click(); // этот локатор мне не нравится
+////        }
+//
+//        $("#fast_login_view").should(exist, Duration.ofSeconds(20));
+//        $("#fast_login_tertiary_btn").click(); // этот локатор мне не нравится
+//
+//        $("#playerContainer").shouldBe(visible);
+//
+//        if ($$("#video_play_button").size() > 0) {
+//            System.out.println("ВИДЕО ИЗНАЧАЛЬНО БЫЛО НЕ ЗАПУЩЕНО");
+//            $("#video_play_button").click();
+//            System.out.println("Запустили");
+//        }
+//        $$("#progress_view").shouldHave(size(0), Duration.ofSeconds(10));
+//
+//        $("#playerContainer").click();
+//        $("#playerContainer").click();
+//        System.out.println("Остановили 2 раза нажали");
+//        $("#video_play_button").shouldBe(visible);
+//        String progress = $("#current_progress").getText();
+//        System.out.println(progress);
+//        float seekBar = Float.parseFloat($("#seek_bar").getText());
+//        $("#video_play_button").click();
+//        System.out.println("запустили");
+//        $$("#video_play_button").shouldHave(size(0), Duration.ofSeconds(5)); // was not correct
+//        Thread.sleep(10000);
+//        $("#playerContainer").click();
+//        $("#playerContainer").click();
+//        System.out.println("Остановили 2 раза нажали");
+//        String progress2 = $("#current_progress").getText();
+//        float seekBar2 = Float.parseFloat($("#seek_bar").getText());
+//        System.out.println(progress2);
+//
+//        int seconds = Integer.parseInt(progress.split("/")[0].trim().split(":")[1]);
+//        int seconds2 = Integer.parseInt(progress2.split("/")[0].trim().split(":")[1]);
+//        Assert.assertTrue(seconds2 - seconds > 2 & seekBar2 - seekBar > 2);
+//    }
+
     @Test(description = "Позитивный сценарий: видео воспроизводится",
             groups = {"Vkvideo"})
-    public void shouldPlaySelectedVideo() throws InterruptedException, MalformedURLException {
+    public void shouldPlaySelectedVideo() throws MalformedURLException {
 
-//        getAppiumDriver().get("https://vkvideo.ru/video-211229778_456240419");
+        VkvideoPage page = new VkvideoPage(getAppiumDriver());
 
-        ((JavascriptExecutor) driver).executeScript("mobile: deepLink", Map.of(
-                "url", "https://vkvideo.ru/video-211229778_456240419",
-                "package", "com.vk.vkvideo"
-        ));
+        int progressBefore =
+                page
+                        .openVideoByDeeplink("https://vkvideo.ru/video-211229778_456240419")
+                        .playFirstOpenedVideo()
+                        .pauseVideo()
+                        .getCurrentProgress();
 
-//        if ($$("#fast_login_view").size() > 0) {
-//            $("#fast_login_tertiary_btn").click(); // этот локатор мне не нравится
-//        }
+        page.playVideo();
 
-        $("#fast_login_view").should(exist, Duration.ofSeconds(20));
-        $("#fast_login_tertiary_btn").click(); // этот локатор мне не нравится
+        Utils.waitExactTime(10);
 
-        $("#playerContainer").shouldBe(visible);
+        int progressAfter =
+                page
+                        .pauseVideo()
+                        .getCurrentProgress();
 
-        if ($$("#video_play_button").size() > 0) {
-            System.out.println("ВИДЕО ИЗНАЧАЛЬНО БЫЛО НЕ ЗАПУЩЕНО");
-            $("#video_play_button").click();
-            System.out.println("Запустили");
-        }
-        $$("#progress_view").shouldHave(size(0), Duration.ofSeconds(10));
-
-        $("#playerContainer").click();
-        $("#playerContainer").click();
-        System.out.println("Остановили 2 раза нажали");
-        $("#video_play_button").shouldBe(visible);
-        String progress = $("#current_progress").getText();
-        System.out.println(progress);
-        float seekBar = Float.parseFloat($("#seek_bar").getText());
-        $("#video_play_button").click();
-        System.out.println("запустили");
-        $$("#video_play_button").shouldHave(size(0), Duration.ofSeconds(5)); // was not correct
-        Thread.sleep(10000);
-        $("#playerContainer").click();
-        $("#playerContainer").click();
-        System.out.println("Остановили 2 раза нажали");
-        String progress2 = $("#current_progress").getText();
-        float seekBar2 = Float.parseFloat($("#seek_bar").getText());
-        System.out.println(progress2);
-
-        int seconds = Integer.parseInt(progress.split("/")[0].trim().split(":")[1]);
-        int seconds2 = Integer.parseInt(progress2.split("/")[0].trim().split(":")[1]);
-        Assert.assertTrue(seconds2 - seconds > 2 & seekBar2 - seekBar > 2);
+        Assert.assertTrue(progressAfter - progressBefore > 1);
     }
 
 
